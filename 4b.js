f086ce4764655bc52dc07ddb6e558b6132b6b2f6
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-12 19:36:30 +0530

    Update 4b.js

--- 4b.js
+++ 4b.js
@@ -19,3 +19,36 @@ app.get('/about.html', function(req, res){
 })
 
  app.listen(5000);
+
+//index.html
+<html>
+<body>
+<a href = "home.html">Home</a>
+<a href = "about.html">About</a>
+<a href = "contact.html">Contact</a>
+</body>
+</html>
+
+//home.html
+<html>
+<body> 
+this is home
+<a href="index.html">Index</a>
+</body
+</html>
+
+//about.html
+<html>
+<body> 
+this is about
+<a href="index.html">Index</a>
+</body
+</html>
+
+//contact.html
+<html>
+<body>
+ this is contact
+<a href="index.html">Index</a>
+</body
+</html>
